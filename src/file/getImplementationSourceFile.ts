33074186a6eb2aed8d045961e98a09c64801ab02
Lars Kappert <lars@webpro.nl>
2022-09-24 17:44:50 +0200

    Some minor internal refactorings

--- src/file/getImplementationSourceFile.ts
+++ src/file/getImplementationSourceFile.ts
@@ -2,14 +2,14 @@ import { ts } from 'ts-morph';
 import type { SourceFile, ImportDeclaration, Identifier } from 'ts-morph';
 import { getExportDeclarationForIdentifier } from '../node/getExportDeclarationForIdentifier';
 
-const getIdentifierSourceFile = (sourceFile: SourceFile, identifier: Identifier): SourceFile | undefined => {
+const getSourceFileForIdentifier = (sourceFile: SourceFile, identifier: Identifier): SourceFile | undefined => {
   const exportDeclaration = getExportDeclarationForIdentifier(sourceFile, identifier);
   const targetFile = exportDeclaration?.getModuleSpecifierSourceFile();
-  return targetFile && targetFile !== sourceFile ? getIdentifierSourceFile(targetFile, identifier) : sourceFile;
+  return targetFile && targetFile !== sourceFile ? getSourceFileForIdentifier(targetFile, identifier) : sourceFile;
 };
 
 export const getImplementationSourceFile = (importDeclaration: ImportDeclaration): SourceFile | undefined => {
   const sourceFile = importDeclaration.getModuleSpecifierSourceFileOrThrow();
   const identifier = importDeclaration.getFirstDescendantByKindOrThrow(ts.SyntaxKind.Identifier);
-  return getIdentifierSourceFile(sourceFile, identifier);
+  return getSourceFileForIdentifier(sourceFile, identifier);
 };
--- src/node/getDeclarationsForSymbols.ts
+++ src/node/getDeclarationsForSymbols.ts
@@ -1,9 +1,8 @@
 import type { Node, Symbol } from 'ts-morph';
 
+const getDeclarationsForSymbol = (symbol: Symbol): Node[] => symbol.getDeclarations();
+
 export const getDeclarationsForSymbols = (symbols: Symbol[]): Node[] => {
-  const declarations = symbols
-    .map(symbol => symbol.getDeclarations())
-    .flat()
-    .filter(Boolean);
+  const declarations = symbols.map(getDeclarationsForSymbol).flat().filter(Boolean);
   return [...new Set(declarations)];
 };
